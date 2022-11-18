0324e22fa72da8c34873cec046cd2f0b95904140
NemesisX1 <junior.medehou00@gmail.com>
2022-11-18 12:14:22 +0100

    (ini) very good package

---
+++ test/src/transfa_dart_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:transfa_dart/transfa_dart.dart';
+
+void main() {
+  group('TransfaDart', () {
+    test('can be instantiated', () {
+      expect(TransfaDart(), isNotNull);
+    });
+  });
+}
