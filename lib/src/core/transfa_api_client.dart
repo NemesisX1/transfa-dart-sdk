c9cd483d905fdb9c8d49f973437b0c01cd4a460e
NemesisX1 <junior.medehou00@gmail.com>
2022-11-18 17:11:13 +0100

    (fix) api version

--- lib/src/core/transfa_api_client.dart
+++ lib/src/core/transfa_api_client.dart
@@ -1,6 +1,5 @@
 import 'package:logger/logger.dart';
 import 'package:transfa_dart/enums/enums.dart';
-import 'package:transfa_dart/src/core/base_model.dart';
 import 'package:transfa_dart/src/helpers/endpoints.dart';
 
 /// basic interface to provide api client for Transfa
@@ -19,7 +18,7 @@ abstract class TransfaApiClient {
   final bool testMode;
 
   /// Transfa's base url
-  final String baseUrl = Endpoints.baseUrl;
+  late String baseUrl = '${Endpoints.baseUrl}${ApiVersion.v1.name}/';
 
   /// api's current version
   final ApiVersion version;
